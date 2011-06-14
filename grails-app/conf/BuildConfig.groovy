40cf69ba7c642a34504fc741d0c3ed4a47a182bb
Noah Sloan <iamnoah@gmail.com>
2011-06-14 11:30:25 -0500

    move resolver into a jar

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,14 +1,5 @@
-import java.io.File;
+import java.io.File; 
 import java.util.List;
-
-import org.apache.ivy.plugins.repository.AbstractRepository;
-import org.apache.ivy.plugins.repository.BasicResource;
-import org.apache.ivy.plugins.repository.Resource;
-import org.apache.ivy.plugins.resolver.DependencyResolver;
-import org.apache.ivy.plugins.resolver.RepositoryResolver;
-import org.apache.ivy.plugins.resolver.packager.BuiltFileResource;
-import org.apache.tools.ant.taskdefs.Jar;
-import org.apache.tools.ant.taskdefs.Manifest;
 
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
@@ -21,143 +12,13 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-	def repo = new AbstractRepository() {
-		
-		final ORG = 'ATG_MODULE'
-		
-		File atg_root
-		
-		File default_jar
-		
-		File getAtgRoot() {
-			atg_root = atg_root ?: resolveAtgRoot() ?: new File(".").absoluteFile
-		}
-		
-		File getDefaultJar() {
-			default_jar = default_jar ?: createDefaultJar()
-		}
-		
-		def resources = [:]
-		
-		protected File createDefaultJar() {
-			Jar task = new Jar()
-			def defaultManifest = File.createTempFile("atg-default",".mf")
-			Manifest.defaultManifest.print(defaultManifest.newPrintWriter())
-			task.manifest = defaultManifest
-			def defaultJar = File.createTempFile("atg-default",".jar")
-			task.destFile = defaultJar
-			task.execute()
-			defaultJar
-		}
-		
-		protected File resolveAtgRoot() {
-			def root = System.getenv("DYNAMO_HOME") ?: System.getenv("ATG_HOME")
-			if(root) {
-				root = new File(root).parentFile
-			}  else {
-				root = new File(".").absoluteFile
-				while(root && !new File(root,"home").with { 
-					it.directory && it.exists()
-				}) {
-					root = root.parentFile
-				}
-			}
-			root
-		}
-		
-		@Override
-		public void get(String src, File dest) throws IOException {
-			def match = src =~ /atg-(.+?)-.*-ivy\.xml/
-			def r 
-			if(match) {
-				r = getResource(match[0][1])
-			} else {
-				match = src =~ /([^\/]+)\/lib\/classes\.jar$/
-				if(match) {
-					r = getResource("${ORG}:${match[0][1]}:any:jar")
-				} else {
-					r = getResource('default-jar')
-				}
-			}
-			fireTransferInitiated(r,5)
-			fireTransferStarted()
-			
-			new URL(src).withInputStream { ins ->
-				dest.withOutputStream { out -> out << ins }
-			}
-			fireTransferProgress(dest.size())
-			fireTransferCompleted(dest.size())
-		}
-		
-		@Override
-		public Resource getResource(String src) throws IOException {
-			if(resources[src]) {
-				return resources[src]
-			}
-			if(src == 'default-jar') {
-				return new BuiltFileResource(defaultJar)
-			}
-			def (org,module,ver,type) = (src.split(/:/) as List)
-			if(org != ORG) {
-				return new BasicResource('not found',false,0,0,false)
-			}
-			File file = new File(atgRoot,"${module}/META-INF/MANIFEST.MF")
-			if(file.exists() && type == "ivy") {
-				File tmp = File.createTempFile("atg-${src}-","-ivy.xml")
-				tmp.withWriter {
-					def build = new groovy.xml.MarkupBuilder(it)
-					build.doubleQuotes = true
-					build.'ivy-module'(version:'2.0') {
-						info(
-								organisation: org,
-								module: module,
-								revision: ver,
-								status: 'release',
-								'default': 'true',
-								)
-						build.configurations() {
-							conf(name:"default",visibility:"public")
-						}
-						build.publications() {
-							artifact(name:module,type:"jar",ext:"jar",conf:"default")
-						}
-						build.dependencies {
-							file.withReader {
-								Manifest m = new Manifest(it)
-								def val = m.getMainSection().getAttributeValue("ATG-Required")
-								val?.split(/\s+/)?.findAll { it }?.each {
-									build.dependency(org:ORG,name:it,rev:ver)
-								}
-							}
-						}
-					}
-					it.close()
-				}
-				resources[src] = new BuiltFileResource(tmp)
-			} else if(file.exists()){
-				File jar = new File(atgRoot,"${module}/lib/classes.jar")
-				if(jar.exists()) {
-					new BuiltFileResource(jar)
-				} else {
-					new BuiltFileResource(defaultJar)
-				}
-			}  else {
-				new BuiltFileResource(file)
-			}
-		}
-		
-		@Override
-		public List<?> list(String parent) throws IOException {
-			return Collections.emptyList();
-		}
-	}
-	
-	def atgResolver = new RepositoryResolver()
-	atgResolver.repository = repo
-	atgResolver.addIvyPattern("[organisation]:[module]:[revision]:[type]:[artifact]:[ext]")
-	atgResolver.addArtifactPattern("[organisation]:[module]:[revision]:[type]:[artifact]:[ext]")
-	atgResolver.name = "ATG Resolver"
-	resolver atgResolver
+
+
+	def myCL = new URLClassLoader([new File(
+				"${basedir}/lib/atg-resolver-1.0-SNAPSHOT.jar"
+			).toURI().toURL()] as URL[],
+			org.apache.ivy.plugins.repository.AbstractRepository.classLoader)
+	resolver myCL.loadClass('com.noahsloan.atg.ivy.AtgModuleRepository').newResolver
 	
     repositories {
         grailsPlugins()
